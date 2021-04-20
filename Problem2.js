2960ab226b641bd758c2bfcc2798992f9a4ba6d2
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-04-19 18:16:31 -0700

    Fixed recursive function

--- Problem2.js
+++ Problem2.js
@@ -9,13 +9,14 @@ var myArray1 = [1, 2, 3, 4, 5, 6, 7, 8];
 var myArray2 = [3, 4, 6, 2, 4, 6];
 var myArray3 = [11, 33, 12, 23];
 
-function findArraySum(arr) {
-    var sum = 0;
-    for(let i of arr)                   //using "for/of" loop here to loop through the values of an iterable objects;
-    sum += i;                           //set up paremeter to add each value insdie the array;
-    return sum;                         //returns the sum of the input array;
-};
+function sumArray(arr) {
+    if(arr.length == 1) {                               //set up our base case for our function to recurse with, arr.length 1 = arr[0];
+    return arr[0];
+    } else {
+    return arr[0] + sumArray(arr.slice([1]));           //function calls upon itself and holds the first value of the array using .slice(), when if condition is true, function will return the sum of all the number;
+    }
+}
 
-console.log(findArraySum(myArray1));    //output 36;
-console.log(findArraySum(myArray2));    //output 25;
-console.log(findArraySum(myArray3));    //output 79;
+console.log(sumArray(myArray1));    //output 36;
+console.log(sumArray(myArray2));    //output 25;
+console.log(sumArray(myArray3));    //output 79;
